da7e49f271135f6005ae27ebef58831fe968f56a
LING TI LEE <134474073+tilee00@users.noreply.github.com>
2025-04-01 09:39:57 +0000

    change datetime

--- bankapp/src/main/java/com/springboot/bankapp/service/AccountService.java
+++ bankapp/src/main/java/com/springboot/bankapp/service/AccountService.java
@@ -2,6 +2,7 @@ package com.springboot.bankapp.service;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
@@ -21,7 +22,7 @@ import com.springboot.bankapp.repository.AccountRepository;
 import com.springboot.bankapp.repository.TransactionRepository;
 
 @Service
-public class AccountService implements UserDetailsService{
+public class AccountService implements UserDetailsService {
 
     @Autowired
     PasswordEncoder passwordEncoder;
@@ -32,14 +33,13 @@ public class AccountService implements UserDetailsService{
     @Autowired
     private TransactionRepository transactionRepository;
 
-    public Account findAccountByUsername(String username){
+    public Account findAccountByUsername(String username) {
         return accountRepository.findByUsername(username)
                 .orElseThrow(() -> new RuntimeException("Account not found"));
     }
 
-    
-    public Account registerAccount(String username, String password){
-        if(accountRepository.findByUsername(username).isPresent()){
+    public Account registerAccount(String username, String password) {
+        if (accountRepository.findByUsername(username).isPresent()) {
             throw new RuntimeException("Username already exists.");
         }
 
@@ -50,18 +50,21 @@ public class AccountService implements UserDetailsService{
         return accountRepository.save(account);
     }
 
-    public void deposit(Account account, BigDecimal amount){
+    public void deposit(Account account, BigDecimal amount) {
         account.setBalance(account.getBalance().add(amount));
         accountRepository.save(account);
 
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        String dateTime = LocalDateTime.now().format(formatter);
+
         Transaction transaction = new Transaction(
-            amount, "Deposit", LocalDateTime.now(), account);
+                amount, "Deposit", dateTime, account);
 
         transactionRepository.save(transaction);
     }
 
-    public void withdraw(Account account, BigDecimal amount){
-        if(account.getBalance().compareTo(amount) < 0){
+    public void withdraw(Account account, BigDecimal amount) {
+        if (account.getBalance().compareTo(amount) < 0) {
             // .compareTo() returns -1 if first value is less than, 0 if equal, and 1 if greater
             throw new RuntimeException("Insufficient funds");
         }
@@ -69,37 +72,40 @@ public class AccountService implements UserDetailsService{
         account.setBalance(account.getBalance().subtract(amount));
         accountRepository.save(account);
 
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        String dateTime = LocalDateTime.now().format(formatter);
+
         Transaction transaction = new Transaction(
-            amount, "Withdrawal", LocalDateTime.now(), account);
+                amount, "Withdrawal", dateTime, account);
         transactionRepository.save(transaction);
     }
 
-    public List<Transaction> getTransactionHistory(Account account){
+    public List<Transaction> getTransactionHistory(Account account) {
         return transactionRepository.findByAccountId(account.getId());
     }
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Account account = findAccountByUsername(username);
-        if(account == null){
+        if (account == null) {
             throw new UsernameNotFoundException("Username or Password not found");
-        }        
+        }
 
         return new Account(
-            account.getUsername(),
-            account.getPassword(),
-            account.getBalance(),
-            account.getTransactions(),
-            authorities()
+                account.getUsername(),
+                account.getPassword(),
+                account.getBalance(),
+                account.getTransactions(),
+                authorities()
         );
     }
 
-    public Collection<? extends GrantedAuthority> authorities(){
+    public Collection<? extends GrantedAuthority> authorities() {
         return Arrays.asList(new SimpleGrantedAuthority("User"));
     }
 
-    public void transferAmount(Account fromAccount, String toUsername, BigDecimal amount){
-        if(fromAccount.getBalance().compareTo(amount) < 0){
+    public void transferAmount(Account fromAccount, String toUsername, BigDecimal amount) {
+        if (fromAccount.getBalance().compareTo(amount) < 0) {
             // .compareTo() returns -1 if first value is less than, 0 if equal, and 1 if greater
             throw new RuntimeException("Insufficient funds");
         }
@@ -113,15 +119,18 @@ public class AccountService implements UserDetailsService{
         toAccount.setBalance(toAccount.getBalance().add(amount));
         accountRepository.save(toAccount);
 
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        String dateTime = LocalDateTime.now().format(formatter);
+
         Transaction debitTransaction = new Transaction(
-            amount, "Transfet Out to " + toAccount.getUsername(),
-            LocalDateTime.now(), fromAccount);        
+                amount, "Transfet Out to " + toAccount.getUsername(),
+                dateTime, fromAccount);
         transactionRepository.save(debitTransaction);
 
         Transaction creditTransaction = new Transaction(
-            amount, "Transfer In to " + fromAccount.getUsername(),
-            LocalDateTime.now(), toAccount);
+                amount, "Transfer In from " + fromAccount.getUsername(),
+                dateTime, toAccount);
         transactionRepository.save(creditTransaction);
-        
+
     }
 }
